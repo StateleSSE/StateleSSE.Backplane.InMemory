1b25633ec76135b0f9881dc99156148b96566a3f
Alex Uldahl Pedersen <alex@uldahl.dk>
2026-01-05 22:09:26 +0100

    initial commit

---
+++ Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+using Microsoft.Extensions.DependencyInjection;
+using StateleSSE.Abstractions;
+using StateleSSE.Backplane.InMemory.Infrastructure;
+
+namespace StateleSSE.Backplane.InMemory.Extensions;
+
+/// <summary>
+/// Extension methods for configuring InMemoryBackplane SSE services
+/// </summary>
+public static class ServiceCollectionExtensions
+{
+    /// <summary>
+    /// Adds in-memory SSE backplane to the service collection.
+    /// Suitable for single-server deployments, development, and testing.
+    /// </summary>
+    /// <param name="services">The service collection</param>
+    /// <returns>The service collection for chaining</returns>
+    public static IServiceCollection AddInMemorySseBackplane(this IServiceCollection services)
+    {
+        services.AddSingleton<InMemoryBackplane>();
+        services.AddSingleton<ISseBackplane>(sp => sp.GetRequiredService<InMemoryBackplane>());
+        return services;
+    }
+}
